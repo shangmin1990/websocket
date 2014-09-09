09c4fd7fca7a9598fd4400f1abe362590e414ce8
Benjamin <benjamin1990@163.com>
2014-09-09 22:15:13 +0800

    add oauth test and webapp

---
+++ src/main/java/com/benjamin/todos/web/controller/OauthTest.java
@@ -0,0 +1,17 @@
+package com.benjamin.todos.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by benjamin on 9/9/14.
+ */
+@Controller
+@RequestMapping("/oauth2")
+public class OauthTest {
+  @RequestMapping("test")
+  public @ResponseBody String test(){
+    return "Oauth2 Hello World";
+  }
+}
